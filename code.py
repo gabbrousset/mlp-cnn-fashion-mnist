2d44ec7bd7e1419ec51b67370904571a010af1f2
gabriel <gabriel.brousset01@gmail.com>
2026-01-08 14:02:41 -0500

    m-chip macs support for mps

--- code.py
+++ code.py
@@ -3,8 +3,6 @@
 
 Gabriel Caballero (261108565)
 Adam Dufour (261193949)
-
-Task 1 - Obtaining Data
 """
 
 import numpy as np
@@ -521,7 +519,7 @@ def evaluate_acc(y_true, y_pred):
   accuracy = np.mean(y_true == y_pred)
   return accuracy
 
-"""# Task 3 - Runnig the experiments
+"""# Task 3 - Running the experiments
 
 ## Baseline Hyperparameters
 We'll be using a baseline MLP:
@@ -993,7 +991,11 @@ plt.show()
 
 """## CNN"""
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
 print(f"Using device: {device}")
 # would be faster if cuda is available
 
